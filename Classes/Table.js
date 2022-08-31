c54bea15e7d186862e04dd4ad852937fda354faa
Andrew <tandrej768@gmail.com>
2022-08-31 14:16:03 +0300

    refactor

--- Classes/Table.js
+++ Classes/Table.js
@@ -1,4 +1,3 @@
-import {EventEmitter} from "./EventEmitter.js";
 import {TableController} from "./TableController.js";
 import {Search} from "./Search.js";
 import {Sort} from "./Sort.js";
@@ -7,6 +6,6 @@ export class Table {
   constructor(id, data) {
     this.container = document.getElementById(id);
     this.data = data;
-    const tableController = new TableController(this.container, this.data, [Search, Sort]);
+    new TableController(this.container, this.data, [Search, Sort]);
   }
 }
